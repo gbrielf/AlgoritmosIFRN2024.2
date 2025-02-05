b58e805ed0ffd385d0950a6a9effaa787391eb3a
Gabriel Henrique F. Barbosa <56904537+gbrielf@users.noreply.github.com>
2025-02-05 13:00:42 +0000

    Adicionando o algoritmo do Selection Sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,44 +1,33 @@
-import java.io.IOException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-class BubbleSort
-{
+class InsertionSort {
     private int[] ordenedList;
     private int lengthList;
 
-    // Construtor da Classe BubbleSort
-    public BubbleSort(int[] ordenedList, int lengthList)
-    {
+   
+    public InsertionSort(int[] ordenedList, int lengthList) {
         this.ordenedList = ordenedList;
         this.lengthList = lengthList;
     }
 
-    public int[] ordenedList()
-    {
-        for(int i = 0; i < lengthList; i++)
-        {
-            for(int j = 0; j < lengthList - 1; j++)
-            {
-                // Troca de Valores
-                if(ordenedList[j] > ordenedList[j+1])
-                {
-                    int aux = ordenedList[j];
-                    // ordenedList.set(j, ordenedList.get(j+1));
-                    ordenedList[j] = ordenedList[j+1];
-                    // ordenedList.set(j+1, aux);
-                    ordenedList[j+1] = aux;
-                }
+    public int[] ordenedList() {
+        for (int i = 1; i < lengthList; i++) {
+            int j = i;
+            while (j > 0 && ordenedList[j] < ordenedList[j - 1]) {
+                int aux = ordenedList[j];
+                ordenedList[j] = ordenedList[j - 1];
+                ordenedList[j - 1] = aux;
+                j--;
             }
         }
-
         return ordenedList;
     }
 }
+
 
 public class Main {
     public static void main(String arg[]) throws IOException
@@ -62,17 +51,17 @@ public class Main {
                 ordenedList[i] = random.nextInt(lengthList);
             }
 
-            BubbleSort BubbleSort = new BubbleSort(ordenedList, lengthList);
+            InsertionSort insertionsort = new InsertionSort(ordenedList, lengthList);
 
             // Medir o Tempo de Processamento do Algoritmo Bubble Sort
             double start = System.currentTimeMillis() / (double)1000;
-            int[] sortedList = BubbleSort.ordenedList();
+            int[] sortedList = insertionsort.ordenedList();
             double end = (System.currentTimeMillis() / (double)1000) - start;
 
             System.out.println(end);
 
             // Gravar as Informações em um Arquivo.txt
-            FileWriter file = new FileWriter("BubleSort1000000", true);
+            FileWriter file = new FileWriter("Insertionsort", true);
             PrintWriter ffile = new PrintWriter(file);
 
             int break_ = 0;
--- Insertionsort.java
+++ Insertionsort.java
@@ -4,24 +4,28 @@ import java.io.PrintWriter;
 import java.util.Random;
 import java.util.Scanner;
 
-class InsertionSort {
+class SelectionSort {
     private int[] ordenedList;
     private int lengthList;
 
    
-    public InsertionSort(int[] ordenedList, int lengthList) {
+    public SelectionSort(int[] ordenedList) {
         this.ordenedList = ordenedList;
-        this.lengthList = lengthList;
+        this.lengthList = ordenedList.length;
     }
 
     public int[] ordenedList() {
-        for (int i = 1; i < lengthList; i++) {
-            int j = i;
-            while (j > 0 && ordenedList[j] < ordenedList[j - 1]) {
-                int aux = ordenedList[j];
-                ordenedList[j] = ordenedList[j - 1];
-                ordenedList[j - 1] = aux;
-                j--;
+        for(int i = 0; i < (lengthList -1); i++){
+            int min = i;
+            for(int j = (i+1); j < lengthList; j++){
+                if(ordenedList[j]<ordenedList[min]){ //caso o valor do índice j seja menor que o valor do indice i,  min recebe o valor de j e adiciona a min
+                    min = j;
+                }  
+                if (i != min){ // se o i 
+                    int aux = ordenedList[i];
+                    ordenedList[i] = ordenedList[min];
+                    ordenedList[min] = aux;
+                }
             }
         }
         return ordenedList;
@@ -51,17 +55,17 @@ public class Main {
                 ordenedList[i] = random.nextInt(lengthList);
             }
 
-            InsertionSort insertionsort = new InsertionSort(ordenedList, lengthList);
+            SelectionSort selectionsort = new SelectionSort(ordenedList);
 
             // Medir o Tempo de Processamento do Algoritmo Bubble Sort
-            double start = System.currentTimeMillis() / (double)1000;
-            int[] sortedList = insertionsort.ordenedList();
-            double end = (System.currentTimeMillis() / (double)1000) - start;
+            double start = System.currentTimeMillis()/ (double)1000 ;
+            int[] sortedList = selectionsort.ordenedList();
+            double end = (System.currentTimeMillis()/ (double)1000) - start;
 
             System.out.println(end);
 
             // Gravar as Informações em um Arquivo.txt
-            FileWriter file = new FileWriter("Insertionsort", true);
+            FileWriter file = new FileWriter("SelectionSort100", true);
             PrintWriter ffile = new PrintWriter(file);
 
             int break_ = 0;
